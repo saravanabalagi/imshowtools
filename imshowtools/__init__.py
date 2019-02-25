36853e2c6997f740d83bb55971264cc693b4fc14
Saravanabalagi R <saravanabalagi@hotmail.com>
2019-02-25 17:13:31 +0000

    Bump version: 0.1.0 → 0.2.0

--- imshowtools/__init__.py
+++ imshowtools/__init__.py
@@ -1,5 +1,5 @@
 # Version of imshowtools
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 
 import numpy as np
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="imshowtools",
-    version="0.1.0",
+    version="0.2.0",
     description="imshowtools contains simplified imshow functions to show multiple images and with other options",
     long_description=README,
     long_description_content_type="text/markdown",
