2a9e6d88be3d163c22cbc9276094d4c7e608db82
Saravanabalagi <saravanabalagi@hotmail.com>
2020-01-17 18:21:27 +0530

    Adding return image types instead of just RGBA

--- imshowtools/helper_functions.py
+++ imshowtools/helper_functions.py
@@ -1,14 +1,10 @@
 import warnings
-import platform
-import tempfile
-from datetime import datetime
-import os
 
 import numpy as np
 from matplotlib import pyplot as plt
-from matplotlib.pyplot import imread
 
 _SUPPORTED_MODES = ['RGB', 'BGR']
+_RETURN_IMAGE_TYPES = ['RGB', 'RGBA', 'ARGB', 'BW', 'L', "BGR", "BGRA", "ABGR"]
 
 
 def _has_one_channel(img):
@@ -55,28 +51,38 @@ def _convert_mode(img, mode=None, index=None):
 
 
 def _imshow_finally(fig, return_image):
-    if not return_image:
+    if return_image is None or return_image is False:
         plt.show()
         return
     else:
-        # workaround for not using numpy, uses disk, slower
-        # return _save_and_retrieve_image()
+        return_type = "RGB"
+        if type(return_image) is bool and return_image is True:
+            return_type = "RGBA"
+        elif type(return_image) is str and return_image in _RETURN_IMAGE_TYPES:
+            return_type = return_image
+        else:
+            warnings.warn(f'`return_image` attribute has to either bool or one of {_RETURN_IMAGE_TYPES}\n'
+                          f'Given return image: {return_image} of type {type(return_image).__name__}\n'
+                          f'Returning the image with default type: {return_type}')
 
-        # direct method, not using disk
         fig.canvas.draw()
         width, height = fig.get_size_inches() * fig.get_dpi()
-        image = np.frombuffer(fig.canvas.tostring_argb(), dtype='uint8').reshape(int(height), int(width), 4)
-        image = image[:, :, [1, 2, 3, 0]]          # convert from argb to rgba
+        image_argb = np.frombuffer(fig.canvas.tostring_argb(), dtype='uint8').reshape(int(height), int(width), 4)
+        image_rgba = image_argb[:, :, [1, 2, 3, 0]]       # convert from argb to rgba
         plt.close(fig)
-        return image
 
+        if return_type == "RGB":
+            return image_rgba[..., :3]
+        elif return_type == "BGR":
+            return image_rgba[..., [2, 1, 0]]
+        elif return_type == "BGRA":
+            return image_rgba[..., [2, 1, 0, 3]]
+        elif return_type == "ABGR":
+            return image_rgba[..., [3, 2, 1, 0]]
+        elif return_type in ["BW", "L"]:
+            # ITU-R 601-2 luma transform
+            return np.dot(image_rgba[..., :3], [0.2989, 0.5870, 0.1140])
+        elif return_type == "ARGB":
+            return image_argb
 
-def _save_and_retrieve_image():
-    tempdir = '/tmp' if platform.system() == 'Darwin' else tempfile.gettempdir()
-    filename = f'{datetime.now().strftime("%Y%m%d_%H%M%S")}.png'
-    filepath = os.path.join(tempdir, filename)
-    plt.savefig(filepath, transparent=True)
-
-    image = imread(filepath)
-    os.remove(filepath)
-    return image
+        return image_rgba
--- imshowtools/imshow_functions.py
+++ imshowtools/imshow_functions.py
@@ -2,11 +2,11 @@ from matplotlib import pyplot as plt
 from typing import Union
 import math
 
-from imshowtools.helper_functions import _convert_mode, _SUPPORTED_MODES, _imshow_finally
+from imshowtools.helper_functions import _convert_mode, _SUPPORTED_MODES, _imshow_finally, _RETURN_IMAGE_TYPES
 
 
 def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None, mode: Union[str, list] = None,
-           window_title: str = None, title: Union[str, list] = None, return_image: bool = False) -> None:
+           window_title: str = None, title: Union[str, list] = None, return_image: Union[bool, str] = False) -> None:
     """
     Shows image loaded by opencv after inverting the order of channels
     Can also be used to show single layer depth image
@@ -17,10 +17,11 @@ def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None
         rows: number of rows to show
         columns: numbers of columns to show
         window_title: window title (not applicable for ipynb notebooks)
-        title: title for the image, or list of titles - one for each image
-        return_image: if True will return an image (numpy hwc array); will not display image
+        title: title for the image, or list of titles, one for each image
+        return_image: if one of ['RGB', 'RGBA', 'ARGB', 'BW', 'L', "BGR", "BGRA", "ABGR"] returns Image.
+                      if True returns 'RGB'. Does not display image if set. if False, returns None, but displays image.
     Returns:
-        None
+        None if return_image is False, else numpy.ndarray of shape [h,w,c] depending on its value.
     """
     plt.rcParams['image.cmap'] = cmap
 
@@ -28,7 +29,7 @@ def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None
     # But matplotlib will print a warning
     # <Figure size 432x288 with 0 Axes>
     fig = None
-    if window_title is not None or return_image is True:
+    if window_title is not None or return_image is True or return_image in _RETURN_IMAGE_TYPES:
         fig = plt.figure(window_title)
 
     title_list = None
@@ -80,8 +81,8 @@ def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None
     return _imshow_finally(fig, return_image)
 
 
-def cvshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None,
-           window_title: str = None, title: Union[str, list] = None) -> None:
+def cvshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None, window_title: str = None,
+           title: Union[str, list] = None, return_image: Union[bool, str] = False) -> None:
     """
     Convenience function for displaying images loaded by OpenCV which are read as BGR by default,
     same as using imshow with `mode='BGR'`
@@ -92,8 +93,10 @@ def cvshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None
         columns: numbers of columns to show
         window_title: window title (not applicable for ipynb notebooks)
         title: title for the image, or list of titles - one for each image
+        return_image: if one of ['RGB', 'RGBA', 'ARGB', 'BW', 'L', "BGR", "BGRA", "ABGR"] returns Image.
+                      if True returns 'RGB'. Does not display image if set. if False, returns None, but displays image.
     Returns:
-        None
+        None if return_image is False, else numpy.ndarray of shape [h,w,c] depending on its value.
     """
     return imshow(*images, cmap=cmap, rows=rows, columns=columns, mode='BGR',
-                  window_title=window_title, title=title)
+                  window_title=window_title, title=title, return_image=return_image)
