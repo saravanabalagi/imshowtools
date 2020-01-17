546ca15f755743e429fd066a279b57b5d35ce5d1
Saravanabalagi <saravanabalagi@hotmail.com>
2020-01-17 19:10:13 +0530

    Default cmap gray, updating return type

--- imshowtools/imshow_functions.py
+++ imshowtools/imshow_functions.py
@@ -1,18 +1,19 @@
 from matplotlib import pyplot as plt
-from typing import Union
+from typing import Union, Any
 import math
 
 from imshowtools.helper_functions import _convert_mode, _SUPPORTED_MODES, _imshow_finally, _RETURN_IMAGE_TYPES
 
 
-def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None, mode: Union[str, list] = None,
-           window_title: str = None, title: Union[str, list] = None, return_image: Union[bool, str] = False) -> None:
+def imshow(*images, cmap: str = 'gray', rows: int = None, columns: int = None,
+           mode: Union[str, list] = None, window_title: str = None, title: Union[str, list] = None,
+           return_image: Union[bool, str] = False) -> Union[None, Any]:
     """
     Shows image loaded by opencv after inverting the order of channels
     Can also be used to show single layer depth image
     Args:
         *images: one of more np.array of shape h,w,c or simple h,w
-        cmap: specify a cmap to apply to all images (viridis by default)
+        cmap: specify a cmap to apply to all images (gray by default)
         mode: specify a mode or color space one in RGB or BGR
         rows: number of rows to show
         columns: numbers of columns to show
@@ -84,14 +85,14 @@ def imshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None
     return _imshow_finally(fig, return_image)
 
 
-def cvshow(*images, cmap: str = 'viridis', rows: int = None, columns: int = None, window_title: str = None,
-           title: Union[str, list] = None, return_image: Union[bool, str] = False) -> None:
+def cvshow(*images, cmap: str = 'gray', rows: int = None, columns: int = None, window_title: str = None,
+           title: Union[str, list] = None, return_image: Union[bool, str] = False) -> Union[None, Any]:
     """
     Convenience function for displaying images loaded by OpenCV which are read as BGR by default,
     same as using imshow with `mode='BGR'`
     Args:
         *images: one of more np.array of shape h,w,c or simple h,w
-        cmap: specify a cmap to apply to all images (viridis by default)
+        cmap: specify a cmap to apply to all images (gray by default)
         rows: number of rows to show
         columns: numbers of columns to show
         window_title: window title (not applicable for ipynb notebooks)
